c2ab6e743edf1208be771716ce3c88ee89f8e90b
guptaaman92 <34260297+guptaaman92@users.noreply.github.com>
2020-02-25 12:48:12 +0530

    version 2.0

---
+++ src/components/Burger/BurgerIngredients/BurgerIngredients.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import classes from './BurgerIngredient.module.css'
+
+
+const burgerIngredient = (props) => {
+
+  let ingredient = null;
+  
+switch(props.type)
+{
+  case ('bread-bottom'):
+ingredint=<div className={classes.}></div>
+
+}
+
+}
+
+
+export default burgerIngredient
